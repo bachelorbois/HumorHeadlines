2c4e8033820b1d7d8037bf50bdc8d5ce2f43e41c
nixen97 <nixen97@gmail.com>
2020-02-03 14:10:33 +0100

    Showcasing the class attributes better in testfeature

--- src/lib/features/testfeature.py
+++ src/lib/features/testfeature.py
@@ -1,11 +1,14 @@
 import numpy as np
 from lib.features import Feature
 from lib.parsing import Headline
+from random import randint
 
 class Testfeat(Feature):
+    FROM = 1
+    TO = 6
 
     @classmethod
     def compute_feature(cls, HL : Headline) -> np.ndarray:
         return np.array(
-            [0, 1, 2, 3, 4, 5, 4 ,3, 2, 1]
+            [randint(cls.FROM, cls.TO), 1, 2, 3, 4, 5, 4 ,3, 2, 1]
         )
