91cd3482c624f3451992e4c985582da050db31fa
kris927b <kristian.smed.noergaard@gmail.com>
2020-02-10 14:51:41 +0100

    Fixed the BERT baseline model. It will now "hopefully" generate great results.

--- src/lib/training/berttraining.py
+++ src/lib/training/berttraining.py
@@ -2,11 +2,13 @@
 import datetime
 
 # Third-party libraries
+import tensorflow as tf
 from tensorflow.keras import Model, callbacks
 
 # Custom libraries
-import lib.parsing as parsing
+from lib.parsing import Headline, read_task1_pb
 from lib.models.bertmodel import create_model
+import os
 
 
 
@@ -14,22 +16,23 @@ class BertTraining:
     DIR = "./headline_regression/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%s")
     LOG_DIR = DIR + '/logs/'
     SAVE_DIR = DIR + '/weights/'
-    BERT_VOCAB = "../models/pre-traied/vocab.txt"
+    BERT_VOCAB = f'{os.getcwd()}/lib/models/pre-trained/vocab.txt'
 
-    def __init__(self, bertie : Model, train_path : str, test_path : str) -> BertTraining:
+    def __init__(self, bertie : Model, train_path : str, test_path : str):
         # The given model (Presumably a BERT model)
         self.bertie = bertie
         
         # Load the vocab
+        Headline.BERT_VECTOR_LENGTH = 30
         with open(self.BERT_VOCAB, 'r') as fd:
-            parsing.Headline.SetBERTVocab(fd)
+            Headline.SetBERTVocab(fd)
 
         # Loading the training
         self.train_data = self.load_data(train_path)
 
     def train(self, epoch, batch_size, validation_split=0.2):
         # Run the model please
-        x_train, y_train = self.train_data.GetBERT(), self.train_data.GetGrades()
+        x_train, y_train = self.train_data.GetBERT(), tf.convert_to_tensor(self.train_data.GetGrades(), dtype=tf.float32)
 
         tensorboard = callbacks.TensorBoard(log_dir=self.LOG_DIR)
         saver = callbacks.ModelCheckpoint(filepath=self.SAVE_DIR+'weights.{epoch:02d}-{val_loss:.2f}.hdf5')
@@ -37,16 +40,15 @@ class BertTraining:
         self.bertie.fit(x=x_train, y=y_train,
                         validation_split=validation_split,
                         batch_size=batch_size,
-                        epoch=epoch,
+                        epochs=epoch,
                         shuffle=True,
-                        callbacks=[tensorboard])
+                        callbacks=[tensorboard, saver])
 
         self.bertie.save_weights(self.SAVE_DIR+'final.hdf5')
 
     @staticmethod
     def load_data(path):
         with open(path, 'rb') as fd:
-            data = parsing.read_task1_pb(fd)
+            data = read_task1_pb(fd)
 
         return data
-
