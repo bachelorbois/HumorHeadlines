1de066ae62d7b05d07d418db6d8e1830a8952cd2
kris927b <kristian.smed.noergaard@gmail.com>
2020-02-17 14:18:29 +0100

    Using sarcasm

--- src/lib/models/humormodel.py
+++ src/lib/models/humormodel.py
@@ -1,21 +1,26 @@
 import tensorflow as tf
-from tensorflow.keras import Model, layers, optimizers, metrics
+from tensorflow.keras import Model, layers, optimizers, metrics, load_model
 import tensorflow_hub as hub
 
 from lib.models.module.functionmodule import sigmoid_3
 from lib.models.layers.elmo import ElmoEmbeddingLayer
 
-def create_HUMOR_model(feature_len : int, embeds : bool = False) -> Model:
+def create_HUMOR_model(feature_len : int, token_len : int, embeds : bool = False) -> Model:
+    pretrained_dir = f'{os.getcwd()}/lib/models/pre-trained/'
     input_features = layers.Input(shape=(feature_len,), dtype='float32', name="feature_input")
+    input_tokens = layers.Input(shape=(token_len,), dtype=tf.string, name="token_input")
+
+    sarcasm = load_model(pretrained_dir + 'sarcasm_model.h5')(input_tokens)
+
+    concat_features = [input_features, sarcasm]
     
     if (embeds):
         input_text = layers.Input(shape=(), dtype=tf.string, name="string_input")
         embed = hub.KerasLayer('https://tfhub.dev/google/tf2-preview/nnlm-en-dim128-with-normalization/1')(input_text)    # Expects a tf.string input tensor.
-        concat = layers.Concatenate()([input_features, embed])
-        dense1 = layers.Dense(128, activation='relu')(concat)
-    else:
-        dense1 = layers.Dense(128, activation='relu')(input_features)
+        concat_features.append(embed)
     
+    concat = layers.Concatenate()(concat_features)
+    dense1 = layers.Dense(128, activation='relu')(concat)
     dense1_dropout = layers.Dropout(0.5)(dense1)
     dense2 = layers.Dense(64, activation='relu')(dense1_dropout)
     dense2_dropout = layers.Dropout(0.5)(dense2)
