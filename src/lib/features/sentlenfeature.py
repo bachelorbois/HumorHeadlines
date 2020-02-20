3db8191a1008fa72fc1231f1a4c3fe2ead0178ab
twan <twan@itu.dk>
2020-02-20 10:16:29 +0100

    smol

--- src/lib/features/sentlenfeature.py
+++ src/lib/features/sentlenfeature.py
@@ -19,12 +19,11 @@ class SentLenFeature(Feature):
     @classmethod
     def compute_feature(cls, HL: Headline) -> np.ndarray:
         s = HL.sentence
-        print(" ".join(s))
         #count for each word not specified in the stop-list
         len_wo_stpwds = sum([1 for w in s if w not in cls.stpwds])
         #len_stpwds = sum([1 for w in s if w in cls.stpwds])
         #check if max length conforms to the original paper standard
-        if len_wo_stpwds > 20:
+        if len_wo_stpwds > cls.max_len:
             print(f"fuck! {len_wo_stpwds} {s}")
         #scale the length such that it exists in the interval: ]0,1]
         scaled_len = len_wo_stpwds/cls.max_len
