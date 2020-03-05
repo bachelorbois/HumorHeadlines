48c313f483aaf0b402aecee0ab0e174aaa20f44a
twan <twan@itu.dk>
2020-03-05 09:21:27 +0100

    init file

--- src/lib/features/__init__.py
+++ src/lib/features/__init__.py
@@ -5,3 +5,5 @@ from lib.features.distancefeature import DistanceFeature
 from lib.features.phoneticfeature import PhoneticFeature
 from lib.features.wordclusterfeature import ClusterFeature
 from lib.features.sentlenfeature import SentLenFeature
+from lib.features.nellkbfeature import NellKbFeature
+from lib.features.relationfeature import RelationFeature
